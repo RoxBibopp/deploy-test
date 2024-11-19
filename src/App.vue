771f7689ece2016d81e992add984d5bd62c28588
RoxBibopp <thibault.malik2@gmail.com>
2024-11-19 09:09:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>Coucou !</h1>
+    <p>CI / CD Test !!</p>
+  </div>
+</template>
+
+<style>
+  #app {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 50px;
+  }
+</style>
